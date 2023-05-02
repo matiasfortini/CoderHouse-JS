16cc688d55b336abe26b680ae1d89d09b89ab368
matiasfortini <matiasfortini88@gmail.com>
2023-05-02 19:36:42 -0300

    Modificaciones 3era Pre-Entrega

--- script.js
+++ script.js
@@ -1,172 +1,141 @@
-let id = 0
-let category
-let nameDesign
-let size
-let colour
-let price
-let stock
-let next = true 
-let selectedProduct
-
-const products = []
-
-const dataEntry = () => {
-    while (next) {
-        id++
-        do {
-            category = prompt("Ingrese la categoría del producto").toLowerCase()
-            if (category != "remera" && category != "buzo" && category != "pantalon" && category != "campera") {
-                alert ("No trabajamos con ese producto")
-            }
-        }while (category != "remera" && category != "buzo" && category != "pantalon" && category != "campera")
-        do {
-            nameDesign = prompt("Ingrese el nombre del producto").toLowerCase()
-            if (nameDesign === "" || nameDesign === " " || nameDesign === null) {
-                alert ("Ingrese un nombre válido para el producto")
-            }
-        }while (nameDesign === "" || nameDesign === " " || nameDesign === null)    
-        do {
-            size = prompt ("Ingrese talle del producto").toUpperCase()    
-            if (size != "S" && size != "M" && size != "L" && size != "XL" && size != "XXL") {
-                alert ("No trabajamos con ese talle")
-            }    
-        }while (size != "S" && size != "M" && size != "L" && size != "XL" && size != "XXL")        
-        do {
-            colour = prompt ("Ingrese color del producto").toLowerCase()    
-            if (colour != "negro" && colour != "blanco" && colour != "gris") {
-                alert ("No trabajamos con ese color")
-            } 
-        }while (colour != "negro" && colour != "blanco" && colour != "gris")    
-        do {
-            stock = prompt ("Ingrese stock del producto")
-            if (stock == null || /\D/.test(stock) || stock == ""){
-                alert ("Ingrese un número entero por favor")
-            }    
-        }while (stock == null || /\D/.test(stock) || stock == "")
-        switch (category) {
-            case "remera":
-                price = 2500
-                break
-    
-            case "buzo":
-                price = 6000
-                break
-            
-            case "pantalon":
-                price = 4500
-                break
-    
-            case "campera":
-                price = 5000
-                break
-    
-            default:
-                price = " "
-        }    
-        products.push(
-            {
-                id:id,
-                categoria:category,
-                nombre: nameDesign,
-                talle:size,
-                color:colour,
-                precio:price,
-                stock:stock 
-            }
-        )
-        next = confirm("Quiere ingresar un nuevo producto?")
-    }
+class Producto {
+  constructor(id, categoria, diseño, color, precio, stock) {
+    this.id = id;
+    this.categoria = categoria;
+    this.diseño = diseño;
+    this.color = color;
+    this.precio = parseFloat(precio);
+    this.stock = stock;
+  }
 }
 
-const searchIdProduct = () => {
-    while (next) {
-        const productId = prompt("Ingrese el ID del producto que quiere seleccionar")
-        if (productId > 0 && productId <= products.length) {
-            selectedProduct = products.find(el => el.id == parseInt(productId))
-            break
-        } else {
-            alert("El ID ingresado no es válido")
-            next = confirm("¿Desea volver a ingresar el ID?")
-        }
-    }
-}
+let idProducto = localStorage.getItem('idProducto') || 1;
+let productos = JSON.parse(localStorage.getItem('productos')) || [];
 
-const modifyProduct = () => {
-    while (next){
-        searchIdProduct()
-        while (next){
-            const fieldToModify = prompt ("Qué campo desea modificar?\n1. Nombre\n2. Precio\n3. Stock")        
-            switch (fieldToModify) {
-                case "1":
-                    let newName
-                    do {
-                        newName = prompt("Ingrese el nombre del producto").toLowerCase()
-                        if (newName === "" || newName === " " || newName === null) {
-                            alert ("Ingrese un nombre válido para el producto")
-                        }
-                    }while (newName === "" || newName === " " || newName === null)
-                    selectedProduct.nombre = newName.toLowerCase()
-                    break
-        
-                case "2":
-                    let newPrice
-                    do {
-                        newPrice = parseFloat (prompt ("Ingrese nuevo precio"))
-                        if (isNaN(newPrice) || newPrice == null || newPrice == ""){
-                            alert ("Ingrese un número por favor")
-                        }    
-                    }while (isNaN(newPrice) || newPrice == null && newPrice == "")
-                    selectedProduct.precio = newPrice
-                    break
-        
-                case "3":
-                    let newStock
-                    do {
-                        newStock = prompt ("Ingrese nuevo stock")
-                        if (newStock == null || /\D/.test(newStock) || newStock == ""){
-                            alert ("Ingrese un número entero por favor")
-                        }    
-                    }while (newStock == null || /\D/.test(newStock) || newStock == "")
-                    selectedProduct.stock = newStock
-                    break
-            }
-            next = confirm ("Quiere modificar otro campo del mismo producto?")
-        }
-    next = confirm ("Desea modificar otro producto?")
-    }
-}
+const mostrarProductos = () => {
+    const contenedorProductos = document.querySelector("#contenedorProductos");
+    contenedorProductos.innerHTML = ""; // Limpiamos el contenedor de productos
+  
+    productos.forEach((producto) => {
+      const tarjetaProducto = document.createElement("div");
+      tarjetaProducto.className = "tarjeta";
+      tarjetaProducto.innerHTML = `
+        <div class="datos">
+            <p>ID: ${producto.id}</p>
+            <p>Categoría: ${producto.categoria}</p>
+            <p>Diseño: ${producto.diseño}</p>
+            <p>Color: ${producto.color}</p>
+            <p>Precio: $${producto.precio}</p>
+            <p>Stock: ${producto.stock}</p>
+        </div>
+        <div class="botones">
+            <button class="editar" data-id="${producto.id}">Editar</button>
+            <button class="eliminar" data-id="${producto.id}">Eliminar</button>
+        </div>
+      `;
+  
+      const btnEliminar = tarjetaProducto.querySelector(".eliminar");
+      btnEliminar.addEventListener("click", (e) => {
+        const idProductoEliminar = parseInt(e.target.getAttribute("data-id"));
+        eliminarProducto(idProductoEliminar);
+      });
+  
+      const btnEditar = tarjetaProducto.querySelector(".editar");
+      btnEditar.addEventListener("click", () => {
+        abrirModalEditar(producto);
+      });
+  
+      contenedorProductos.append(tarjetaProducto);
+    });
+};  
 
-const deleteProduct = () => {
-    while (next){
-        searchIdProduct()
-        if (selectedProduct){
-            next = confirm ("Está seguro que quiere eliminar el producto?")
-            const productToDelete = products.indexOf(selectedProduct)
-            products.splice(productToDelete,1)
-        }       
-        next = confirm ("Desea eliminar otro producto?") 
-    }
-}
+const crearProducto = () => {
+  const formularioCrear = document.querySelector('#crearProducto');
+  formularioCrear.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const datos = e.target.children;
+    const producto = new Producto(
+      idProducto,
+      datos['categoria'].value,
+      datos['diseño'].value,
+      datos['color'].value,
+      datos['precio'].value,
+      datos['stock'].value
+    );
+    productos.push(producto);
+    localStorage.setItem('productos', JSON.stringify(productos));
+    formularioCrear.reset();
+    idProducto++;
+    localStorage.setItem('idProducto', idProducto);
+    mostrarProductos()
+  });
+};
 
-const startApp = () => {
-    console.log (products)
-    while(next){
-        let start = prompt ("Qué proceso desea realizar?\n1. Ingresar nuevo producto\n2. Modificar producto\n3. Borrar producto")        
-        switch (start){
-            case "1":
-                dataEntry()
-                break
-                    
-            case "2":
-                modifyProduct()
-                break
+const abrirModalEditar = (producto) => {
+    const modal = document.querySelector("#modal");
+    modal.style.display = "block";
+  
+    // Obtener los elementos del formulario del modal
+    const form = document.querySelector("#editarProducto");
+    const idProductoEditar = document.querySelector("#idProductoEditar");
+    const categoriaEditar = document.querySelector("#categoriaEditar");
+    const diseñoEditar = document.querySelector("#diseñoEditar");
+    const colorEditar = document.querySelector("#colorEditar");
+    const precioEditar = document.querySelector("#precioEditar");
+    const stockEditar = document.querySelector("#stockEditar");
+  
+    // Establecer los valores de los campos del formulario con los datos del producto
+    idProductoEditar.value = producto.id;
+    categoriaEditar.value = producto.categoria;
+    diseñoEditar.value = producto.diseño;
+    colorEditar.value = producto.color;
+    precioEditar.value = producto.precio;
+    stockEditar.value = producto.stock;
+  
+    // Agregar evento 'submit' al formulario de edición
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const datos = e.target.elements;
+        const idProducto = parseInt(datos["idProducto"].value);
+        const categoria = datos["categoria"].value;
+        const diseño = datos["diseño"].value;
+        const color = datos["color"].value;
+        const precio = parseFloat(datos["precio"].value);
+        const stock = parseInt(datos["stock"].value);
+        editarProducto(idProducto, categoria, diseño, color, precio, stock);
+        modal.style.display = "none";
+    });
 
-            case "3":
-                deleteProduct()
-                break
-            }
-        next = confirm("Quiere volver al menú principal?") 
-    }
-}
+  
+    const editarProducto = (idProducto, categoria, diseño, color, precio, stock) => {
+    // Buscar el producto en el array 'productos' y actualizar sus valores
+    productos = productos.map((producto) => {
+      if (producto.id === idProducto) {
+        return new Producto(idProducto, categoria, diseño, color, precio, stock);
+      } else {
+        return producto;
+      }
+    });
 
-startApp()
+    const btnCerrarModal = document.querySelector('#cerrarModal');
+    btnCerrarModal.addEventListener('click', () => {
+        const modal = document.querySelector("#modal");
+        modal.style.display = "none";
+    });
+  
+    // Guardar el array 'productos' en el almacenamiento local (localStorage)
+    localStorage.setItem("productos", JSON.stringify(productos));
+  
+    // Mostrar los productos actualizados en la pantalla
+    mostrarProductos();
+    };
+};     
+
+const eliminarProducto = (idProducto) => {
+    productos = productos.filter((producto) => producto.id !== idProducto);
+    localStorage.setItem("productos", JSON.stringify(productos));
+    mostrarProductos();
+};
+
+mostrarProductos()
+crearProducto();
